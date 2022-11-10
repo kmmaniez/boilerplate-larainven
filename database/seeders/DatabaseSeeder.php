56ed164af81a8172d53ac07eac86057698fda7dc
kmmaniez <dzlabsid@gmail.com>
2022-11-10 15:15:37 +0700

    Seeder database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,7 +33,7 @@ class DatabaseSeeder extends Seeder
         $products = ['Baju muslim','Celana chinos','Hoodie P&B','Jacket Varsitiy','Converse'];
         for ($i=0; $i < count($category); $i++) { 
             \App\Models\Product::factory()->create([
-                'id_kategori' => rand(1,5),
+                'id_kategori' => $i + 1,
                 'nama_produk' => $products[$i],
                 'harga_produk' => rand(2,6) * 3500,
                 'stok' => rand(5,20),

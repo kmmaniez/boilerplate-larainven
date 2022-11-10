0cd6f0976d4cbf6c6081d89c4c7c8bf993241814
kmmaniez <dzlabsid@gmail.com>
2022-11-10 15:15:09 +0700

    Routes application

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Products;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', Products::class);
+Route::resource('products', ProductsController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
